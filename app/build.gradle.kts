bf5509049f02041e41ada6a78bd7e9474209f7d5
Josh Stagg <jstagg@slack-corp.com>
2025-03-07 10:27:46 -0800

    Add shared elements, enable minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
   buildTypes {
     release {
-      isMinifyEnabled = false
+      isMinifyEnabled = true
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
@@ -46,6 +46,7 @@ dependencies {
   implementation(libs.androidx.ui.graphics)
   implementation(libs.androidx.ui.tooling.preview)
   implementation(libs.androidx.material3)
+  implementation(libs.circuit.sharededelements)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/samplesafecastcrash/MainActivity.kt
+++ app/src/main/java/com/example/samplesafecastcrash/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,18 +13,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.samplesafecastcrash.ui.theme.SampleSafeCastCrashTheme
+import com.slack.circuit.sharedelements.SharedElementTransitionLayout
 
 class MainActivity : ComponentActivity() {
+  @OptIn(ExperimentalSharedTransitionApi::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-      SampleSafeCastCrashTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
+      SharedElementTransitionLayout {
+        SampleSafeCastCrashTheme {
+          Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+              name = "Android",
+              modifier = Modifier.padding(innerPadding)
+            )
+          }
         }
       }
     }
